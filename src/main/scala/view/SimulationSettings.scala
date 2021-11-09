a343f4339c385493cab7d676933459b92c74201e
christvalt <valteschrist@gmail.com>
2021-11-09 14:20:31 +0100

    Create SimulationSettings.scala

---
+++ src/main/scala/view/SimulationSettings.scala
@@ -0,0 +1,14 @@
+package view
+
+case class SimulationSettings(
+                               coloniesNumber:Int,
+                               timeGgranuarity:Int,
+                               simalationDuration:Int,
+                               simalationDurationc:Int,
+                               simalationDurations:Int,
+                               simalationDurationcc:Int,
+                               simalationDurationsc:Int,
+                               simalationDurationr:Int,
+                               simalationDurationre:Int,
+                               environementSize:(Int,Int)
+                             )
