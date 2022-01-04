116a9e266469583a4c58dee79a9a530c605f7df5
christvalt <valteschrist@gmail.com>
2022-01-04 02:27:27 +0100

    finishing

--- src/main/scala/model/creature/Collidable.scala
+++ src/main/scala/model/creature/Collidable.scala
@@ -3,11 +3,20 @@ package model.creature
 import model.creature.Behavior.{Simulable, SimulableEntity}
 import model.creature.CreatureObject.Creature
 
-//collidable interface with it's compagnio object  tht represent the creature that collide in the system
+/** a trait that defines the ability of update state, i.e. how a type extending this trait reacts when
+ * calls updateState on it, once per iteration.
+ */
 trait  Collidable{
-
+  /**
+   * Difines how a entity react in response to an collision notfication .
+   * params:the World at the current iteration of the simulation
+   * Returns: A set of simulableEntity
+   * */
   def collision(other:SimulableEntity):Set [SimulableEntity]
 }
+/**collidable interface with it's compagnon
+ *  object  tht represent the creature that
+ *  collide in the system*/
 
 object Collidable {
   trait NeutralCollidable extends Simulable {
--- src/main/scala/model/reaction/BeingEatenEffect.scala
+++ src/main/scala/model/reaction/BeingEatenEffect.scala
@@ -1,6 +1,6 @@
 package model.reaction
 
-import model.SimulationObjectImpl.{ButterflyImpl, EggsImpl, FlourPlant, LarvaImpl, PuppaImpl}
+import model.SimulationObjectImpl.{ButterflyImpl, EggsImpl, FlowerPlant, LarvaImpl, PuppaImpl}
 import model.creature.CreatureObject.Creature
 import utils.TypeUtilities.Life
 
@@ -13,8 +13,8 @@ object BeingEatenEffect {
     case creture: PuppaImpl =>creture.life - REDUCE_LIFE
     case creture :LarvaImpl=>creture.life - REDUCE_LIFE
     case creture :ButterflyImpl=>creture.life - REDUCE_LIFE
-    case creture :FlourPlant=>creture.life - REDUCE_LIFE
-    case creture:FlourPlant=>creture.life - REDUCE_LIFE
+    case creture :FlowerPlant=>creture.life - REDUCE_LIFE
+    case creture:FlowerPlant=>creture.life - REDUCE_LIFE
   }
 
 }
